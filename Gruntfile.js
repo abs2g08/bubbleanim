c0d7c85276473ae0079275a973c8662700def812
Adam Smith <adamsmith@Adams-iMac.local>
2014-11-19 22:20:05 +0000

    added tidied up grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,38 +3,14 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    sass: {                            
-      dist: {                       
+    compass: {
+      dist: {                   
         options: {
-          style: 'expanded'
-        },
-        files: {
-          'dist/style.css': 'scss/style.scss',
-        }
-      }
-    },
-    compass: {                  // Task
-      dist: {                   // Target
-        options: {              // Target options
           sassDir: 'sass',
           cssDir: 'css',
         }
       }
     },
-
-    concat: {
-      options: {
-        // define a string to put between each file in the concatenated output
-        separator: ';'
-      },
-      dist: {
-        // the files to concatenate
-        src: ['js/**/*.js'],
-        // the location of the resulting JS file
-        dest: 'js/app.min.js'
-      }
-    },
-
     requirejs: {
       app: {
         options: {
@@ -46,18 +22,6 @@ module.exports = function(grunt) {
           include:  ['vendor/modernizr-2.6.2.min.js'],
           // optimize : 'uglify',
           optimize: 'none'
-        }
-      }
-    },
-
-    uglify: {
-      options: {
-        // the banner is inserted at the top of the output
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
-      },
-      dist: {
-        files: {
-          'js/app.min.js': ['js/app.min.js']
         }
       }
     },
@@ -82,8 +46,6 @@ module.exports = function(grunt) {
   });
 
   grunt.loadNpmTasks('grunt-contrib-compass');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-preprocess');
   grunt.loadNpmTasks('grunt-contrib-requirejs');
